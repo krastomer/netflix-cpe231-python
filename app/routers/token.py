d1c1b6ee0ec2f96787930b9dfc88aae49aa3084c
gimmeabeer <53146540+krastomer@users.noreply.github.com>
2021-04-25 00:50:09 +0700

    delete print()

--- app/routers/token.py
+++ app/routers/token.py
@@ -49,5 +49,4 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         data={'sub': user.email},
         expires_delta=access_token_expires
     )
-    print(access_token)
     return {'access_token': access_token, 'token_type': 'bearer'}
