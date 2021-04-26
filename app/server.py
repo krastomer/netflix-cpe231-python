a444c3fc1530c93c1ccdf0e890f2f928c56f3d63
gimmeabeer <53146540+krastomer@users.noreply.github.com>
2021-04-26 11:58:04 +0700

    remove doc_url

--- app/server.py
+++ app/server.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from .routers import token, user, poster
 
-app = FastAPI()
+app = FastAPI(
+    title='Netflix CPE231 API',
+    description='This project for CPE231 Database System used in for education',
+    docs_url=None
+)
 
 app.include_router(token.router)
 app.include_router(user.router)
